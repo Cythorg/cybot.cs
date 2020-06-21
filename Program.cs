ae0ac305e52986aae95498ea6b2b451b319bd11a
Cythorg <30748953+Cythorg@users.noreply.github.com>
2020-06-21 13:18:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CybotSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
